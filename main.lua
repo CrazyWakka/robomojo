bc50f96cdb735b76e623aa5226ed9dbff2d1ebc0
Andy Guice <andy_guice@hotmail.com>
2020-04-22 08:37:09 -0500

    initial commit

---
+++ main.lua
@@ -0,0 +1,69 @@
+--[[
+    GD50
+    Flappy Bird Remake
+
+    Author: Colton Ogden
+    cogden@cs50.harvard.edu
+
+    A mobile game by Dong Nguyen that went viral in 2013, utilizing a very simple 
+    but effective gameplay mechanic of avoiding pipes indefinitely by just tapping 
+    the screen, making the player's bird avatar flap its wings and move upwards slightly. 
+    A variant of popular games like "Helicopter Game" that floated around the internet
+    for years prior. Illustrates some of the most basic procedural generation of game
+    levels possible as by having pipes stick out of the ground by varying amounts, acting
+    as an infinitely generated obstacle course for the player.
+]]
+
+require 'src/Dependencies'
+
+-- physical screen dimensions
+WINDOW_WIDTH = 1280
+WINDOW_HEIGHT = 720
+
+-- virtual resolution dimensions
+VIRTUAL_WIDTH = 512
+VIRTUAL_HEIGHT = 288
+
+-- images we load into memory from files to later draw onto the screen
+
+
+function love.load()
+    -- initialize our nearest-neighbor filter
+    love.graphics.setDefaultFilter('nearest', 'nearest')
+
+    math.randomseed(os.time())
+
+    -- app window title
+    love.window.setTitle('Robo Mojo')
+
+    -- load up our global textures
+    gTextures = {
+        ['robot_hero'] = love.graphics.newImage('graphics/BeepBoop2.png')
+    }
+
+    -- initialize our virtual resolution
+    push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
+        vsync = true,
+        fullscreen = false,
+        resizable = true
+    })
+end
+
+function love.resize(w, h)
+    push:resize(w, h)
+end
+
+function love.keypressed(key)
+    if key == 'escape' then
+        love.event.quit()
+    end
+end
+
+function love.draw()
+    push:start()
+    
+    -- render our robot
+    love.graphics.draw(gTextures['robot_hero'])
+
+    push:finish()
+end
